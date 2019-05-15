5dbca83cc8aceb6b66b0b4e91106cd977f51636a
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-05-15 19:10:46 +0200

    Classe acabada

--- M3_POO/src/exercici2.java
+++ M3_POO/src/exercici2.java
@@ -2,25 +2,26 @@ import java.io.*;
 
 public class exercici2 {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		int A, B;
-		
+
 		try {
 			System.out.print("Escriu un número entre 1 i 100: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String n1 = reader.readLine();
 			A = Integer.parseInt(n1);
-	       
+
 			System.out.print("Escriu un altre número entre 1 i 100: ");
 			String n2 = reader.readLine();
 			B = Integer.parseInt(n2);
-			
-			int total = A%B;
-			
-			multiple(A, B);
-			
+
+			int total = A % B;
+
+			multiple(A);
+			interval(A, B);
+
 			System.out.println("El residu de la divisió entre " + A + " i " + B + " és: " + total);
-		
+
 		} catch (Mult10Exception multiplo) {
 			System.out.println("Error! El dividend introduït no és múltiple de 10.");
 		} catch (ForaIntervalException rango) {
@@ -28,12 +29,15 @@ public class exercici2 {
 		}
 	}
 
-	public static void multiple(int A, int B)throws Mult10Exception {
-		if (A%10 != 0) {
+	public static void multiple(int A) throws Mult10Exception {
+		if (A % 10 != 0) {
 			throw new Mult10Exception();
 		}
-	public static void interval()throws ForaIntervalException {
-		
+	}
+
+	public static void interval(int A, int B) throws ForaIntervalException {
+		if ((A < 1 || A > 100) || (B < 1 || B > 100)) {
+			throw new ForaIntervalException();
 		}
 	}
 }
